1b85d9d732f0fa084ad7a80218ca8b4d9069ba6f
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-06 13:12:06 +0300

    добавили проект Example009_Проверка_соответствия_числа_и_его_квадрата

---
+++ Example009_Проверка_соответствия_числа_и_его_квадрата/Program.cs
@@ -0,0 +1,15 @@
+﻿//приглашение к вводу, ввод двух чисел
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+//проверка двух чисел. Является ли первое квадратом второго
+if (a == b * b)
+{
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else
+{
+    Console.WriteLine("Первое число не является квадратом второго числа");
+}
