d6f7ca6811dabdd35b100e9adfc55ae6195bddea
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-06 12:25:19 +0300

    добавили проект Example008_Square

---
+++ Example008_Square/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32( Console.ReadLine() );
+int answer = number * number;
+Console.WriteLine("1 вариант. Квадрат числа " + number + " равен " + answer);
+Console.WriteLine($"2 вариант. Квадрат числа {number} равен {answer}");
