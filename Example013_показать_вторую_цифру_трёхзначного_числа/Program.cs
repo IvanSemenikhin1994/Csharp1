93a6a13a9e5f83e563cbbf533040c9dd4abab66d
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-07 14:59:54 +0300

    добавили решение через элемент строки char в Example013_показать_вторую_цифру_трёхзначного_числа

--- Example013_показать_вторую_цифру_трёхзначного_числа/Program.cs
+++ Example013_показать_вторую_цифру_трёхзначного_числа/Program.cs
@@ -3,12 +3,13 @@
 // 782 -> 8 
 // 918 -> 1
 
+//через число int
 
+/*
 int SecondDigitOfNumber(int number)
 {
-    int SecondDigit = number / 10;
-    int SecondDigit1 = SecondDigit % 10;
-    return SecondDigit1;
+    int SecondDigit = number / 10 % 10;
+    return SecondDigit;
 }
 
 System.Console.WriteLine("Введите трёхзначное число: ");
@@ -17,3 +18,21 @@ if (number > 99 && number < 1000)
     System.Console.WriteLine(SecondDigitOfNumber(number));
 else
     System.Console.WriteLine("Ошибка ввода. Введите трёхзначное число");
+*/
+
+//через элемент строки char
+
+string SecondDigitOfNumber(string text)
+{
+    string secondNumber = Convert.ToString(text[1]);
+    return secondNumber;
+}
+
+System.Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string text = Convert.ToString(number);
+
+if (text.Length != 3)
+    System.Console.WriteLine("Ошибка ввода. Введите трёхзначное число");
+else
+    System.Console.WriteLine(SecondDigitOfNumber(text));
