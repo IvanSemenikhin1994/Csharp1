db03626a16765f81423449140e9004de72b16211
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-07 14:07:55 +0300

    добавили проект Example013_показать_вторую_цифру_трёхзначного_числа

---
+++ Example013_показать_вторую_цифру_трёхзначного_числа/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5 
+// 782 -> 8 
+// 918 -> 1
+
+
+int SecondDigitOfNumber(int number)
+{
+    int SecondDigit = number / 10;
+    int SecondDigit1 = SecondDigit % 10;
+    return SecondDigit1;
+}
+
+System.Console.WriteLine("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(SecondDigitOfNumber(number));
+
