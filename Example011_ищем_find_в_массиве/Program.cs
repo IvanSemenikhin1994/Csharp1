25c9aa2f73c2d05f02166d9920cdf6feb4277dd5
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-07 11:39:33 +0300

    добавили изменения в проект Example011_ищем_find_в_массиве

--- Example011_ищем_find_в_массиве/Program.cs
+++ Example011_ищем_find_в_массиве/Program.cs
@@ -22,10 +22,10 @@ void FillArray(int[] collection)
 int length = collection.Length;
 int index = 0;
 while (index < length)
-{
-    collection[index] = new Random().Next(1, 10);
+    {
+    collection[index] = new Random().Next(1, collection.Length);
     index++;
-}
+    }
 }
 
 void PrintArray(int[] col)
@@ -34,11 +34,34 @@ void PrintArray(int[] col)
     int position = 0;
     while (position < count)
     {
-        Console.WriteLine(position);
+        Console.WriteLine(col[position]);
         position++;
     }
 }
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
+
 FillArray(array);
 PrintArray(array);
+System.Console.WriteLine();
 
+int pos = IndexOf(array, 7);
+System.Console.WriteLine(pos);
