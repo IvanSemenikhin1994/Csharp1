4b0f905404cb5b47b38b5870f44bb60947bb627a
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-07 17:05:42 +0300

    добабили домашнее задание Урок 2. Базовые алгоритмы

---
+++ Урок 2. Базовые алгоритмы/Задача_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// 123456789 -> 3
+
+
+
+System.Console.WriteLine("Введите число: ");
+string numText = Console.ReadLine();
+
+if (numText.Length < 3)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    System.Console.WriteLine(numText[2]);
+}
---
+++ Урок 2. Базовые алгоритмы/Задача_15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день
+// выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Выберите день недели: ");
+Console.WriteLine("1 - понедельник");
+Console.WriteLine("2 - вторник");
+Console.WriteLine("3 - среда");
+Console.WriteLine("4 - четверг");
+Console.WriteLine("5 - пятница");
+Console.WriteLine("6 - суббота");
+Console.WriteLine("7 - воскресенье");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 5)
+{
+    Console.WriteLine("Да, это выходной");
+}
+else
+{
+    Console.WriteLine("Нет, это будний день");
+}
