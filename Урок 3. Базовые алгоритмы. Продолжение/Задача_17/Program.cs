3bfcae863e40c6308d800d2c37fcc2ca630c8a08
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-08 11:59:32 +0300

    добавили задача_17 в Урок 3. Базовые алгоритмы. Продолжение

---
+++ Урок 3. Базовые алгоритмы. Продолжение/Задача_17/Program.cs
@@ -0,0 +1,80 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится
+// эта точка.
+
+
+int NumberQuaterOfPlane(int x1, int y1)
+{
+
+    int result = 0;
+
+    if (x1 == 0 && y1 == 0)
+    {
+        System.Console.WriteLine("Ошибка. x и y не могут быть равны нулю");
+        return result;
+    }
+    else
+    {
+        if (x1 > 0 && y1 > 0)
+        {
+            result = 1;
+            return result;
+        }
+        if (x1 < 0 && y1 > 0)
+        {
+            result = 2;
+            return result;
+        }
+        if (x1 < 0 && y1 < 0)
+        {
+            result = 3;
+            return result;
+        }
+        if (x1 > 0 && y1 < 0)
+        {
+            result = 4;
+            return result;
+        }
+        return result;
+    }
+
+
+    /*
+
+    int result = 0;
+
+    if (x1 > 0 && y1 > 0)
+    {
+        result = 1;
+        return result;
+    }
+    if (x1 < 0 && y1 > 0)
+    {
+        result = 2;
+        return result;
+    }
+    if (x1 < 0 && y1 < 0)
+    {
+        result = 3;
+        return result;
+    }
+    if (x1 > 0 && y1 < 0)
+    {
+        result = 4;
+        return result;
+    }
+
+    System.Console.WriteLine("Ошибка. x и y не могут быть равны нулю");
+    return result;
+
+    */
+
+}
+
+
+System.Console.WriteLine("Введите x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine(NumberQuaterOfPlane(x, y));
