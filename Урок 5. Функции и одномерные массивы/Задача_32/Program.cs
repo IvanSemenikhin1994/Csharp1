0988c7adbe6a656844b5a0de221f33fb24d033f4
iv.semenikhin <iv.semenikhin@gmail.com>
2023-09-15 13:06:42 +0300

    добавили Задача_32 в Урок 5. Функции и одномерные массивы

---
+++ Урок 5. Функции и одномерные массивы/Задача_32/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 32: Напишите программу замена элементов
+// массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArray(int n, int minValue, int maxValue)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+void SwapNumeralSign(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1; 
+    }
+}
+
+
+int[] arr = CreateArray(n:12, minValue:-9, maxValue:9);
+PrintArray(arr);
+SwapNumeralSign(arr);
+System.Console.WriteLine("");
+PrintArray(arr);
