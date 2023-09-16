1ed441da063d18aef2868d15477f3aab22f44cc4
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-16 13:51:53 +0300

    добавили Задача_38 в домашнее задание к Урок 5. Функции и одномерные массивы

--- Урок 5. Функции и одномерные массивы/Домашнее_задание/Задача_38/Program.cs
+++ Урок 5. Функции и одномерные массивы/Домашнее_задание/Задача_38/Program.cs
@@ -2,3 +2,64 @@
 // массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
+double[] CreateArray(int n)
+{
+    double[] array = new double[n];
+    return array;
+}
+
+void FillArray(double[] array, int minValue, int maxValue)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().NextDouble() * ((double) maxValue - minValue)  + minValue;
+    }
+}
+
+void PrintArray(double[] array)
+{
+    foreach (double item in array)
+    {
+        System.Console.WriteLine(item);
+    }
+}
+
+double FindMax(double[] array)
+{
+    double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+    return max;
+}
+
+double FindMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    return min;
+}
+
+int n = 7;
+int minValue = 10;
+int maxValue = 20;
+
+double[] arr = CreateArray(n);
+FillArray(arr, minValue, maxValue);
+PrintArray(arr);
+
+System.Console.WriteLine("");
+System.Console.WriteLine($"Максимальный элемент: {FindMax(arr)}");
+System.Console.WriteLine($"Минимальный элемент: {FindMin(arr)}");
+System.Console.WriteLine($"Разница между максимальным и минимальным элементом: {(FindMax(arr) - FindMin(arr)):f}");
+System.Console.WriteLine("");
