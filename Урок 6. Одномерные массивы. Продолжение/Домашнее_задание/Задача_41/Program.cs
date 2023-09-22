917e13d3bedf897ce29d38e37c97d121fe35cb85
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-22 12:27:06 +0300

    добавили Домашнее_задание/Задача_41 в Урок 6. Одномерные массивы. Продолжение

---
+++ Урок 6. Одномерные массивы. Продолжение/Домашнее_задание/Задача_41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл
+// пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write(item + " ");
+    }
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count += 1;
+    }
+    return count;
+}
+
+int m = 10;
+
+System.Console.WriteLine("Введите числа:");
+int[] numbers = new int[m];
+
+System.Console.WriteLine();
+FillArray(numbers);
+System.Console.WriteLine();
+PrintArray(numbers);
+
+System.Console.WriteLine();
+System.Console.WriteLine($"Количество положительных чисел: {CountPositive(numbers)}");
+
