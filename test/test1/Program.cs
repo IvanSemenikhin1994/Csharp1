c7a870a1f5def849295ec0b62db19fbb6ff633ef
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-29 14:58:48 +0300

    добавили test

---
+++ test/test1/Program.cs
@@ -0,0 +1,55 @@
+﻿void PrintTwoDimArray(int[,] array)
+{
+    // индекс столбцов
+    // for (int i = 0; i < array.GetLength(1); i++)
+    // {
+    //     System.Console.Write("\t" + i);
+    // }
+    // System.Console.WriteLine();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        // System.Console.Write(i + "\t"); // индекс строк
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] CreateTwoDimArray(int rows, int cols, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+int CountAmountOfNums(int[,] matrix, int value)
+{
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == value)
+                count++;
+        }
+    }
+    return count;
+}
+
+int minValue = 0;
+int maxValue = 9;
+
+int[,] matrix = CreateTwoDimArray(rows:6, cols:6, minValue, maxValue);
+PrintTwoDimArray(matrix);
+
+System.Console.WriteLine(CountAmountOfNums(matrix, value:4));
