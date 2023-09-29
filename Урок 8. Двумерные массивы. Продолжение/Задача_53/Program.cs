4bb801fb26270804cd5c7a92e67f1fc4a44e1db0
Ivan Semenikhin <iv.semenikhin@gmail.com>
2023-09-29 12:30:39 +0300

    Добавили Задача_53 и Задача_55 в Урок 8. Двумерные массивы. Продолжение

---
+++ Урок 8. Двумерные массивы. Продолжение/Задача_53/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и последнюю строку
+// массива. Модификация: любые заданные строчки массива
+
+void PrintTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] CreateTwoDimArray(int rows = 4, int cols = 5, int minValue = 0, int maxValue = 9)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+// void ChangeTwoDimArrayV1(int[,] twoDimArray)
+// {
+//     for (int j = 0; j < twoDimArray.GetLength(1); j++)
+//     {
+//         int temp = twoDimArray[0, j];
+//         twoDimArray[0, j] = twoDimArray[twoDimArray.GetLength(0) - 1, j];
+//         twoDimArray[twoDimArray.GetLength(0) - 1, j] = temp;
+//     }
+// }
+
+void ChangeTwoDimArrayV2(int[,] twoDimArray, int s1, int s2)
+{
+    for (int j = 0; j < twoDimArray.GetLength(1); j++)
+    {
+        int temp = twoDimArray[s1 - 1, j];
+        twoDimArray[s1 - 1, j] = twoDimArray[s2 - 1, j];
+        twoDimArray[s2 - 1, j] = temp;
+    }
+}
+
+int[,] twoDimArr = CreateTwoDimArray();
+PrintTwoDimArray(twoDimArr);
+
+System.Console.WriteLine("Выберите строки: ");
+int s1 = int.Parse(Console.ReadLine()!);
+int s2 = int.Parse(Console.ReadLine()!);
+
+ChangeTwoDimArrayV2(twoDimArr, s1, s2);
+PrintTwoDimArray(twoDimArr);
---
+++ Урок 8. Двумерные массивы. Продолжение/Задача_55/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае,
+// если это невозможно, программа должна вывести сообщение для пользователя.
+
+void PrintTwoDimArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int[,] CreateTwoDimArray(int rows = 5, int cols = 5, int minValue = 0, int maxValue = 9)
+{
+    int[,] array = new int[rows, cols];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void ChangeRowsToCols(int[,] twoDimArray)
+{
+    int pivot = 0;
+    int maxLength = twoDimArray.GetLength(0);
+    if (maxLength > twoDimArray.GetLength(1))
+        maxLength = twoDimArray.GetLength(1);
+    for (pivot = 0; pivot < maxLength; pivot++)
+    {
+        for (int i = pivot + 1; i < maxLength; i++)
+        {
+            int temp = twoDimArray[pivot, i];
+            twoDimArray[pivot, i] = twoDimArray[i, pivot];
+            twoDimArray[i, pivot] = temp;
+        }
+    }
+}
+
+int[,] twoDimArr = CreateTwoDimArray();
+PrintTwoDimArray(twoDimArr);
+
+ChangeRowsToCols(twoDimArr);
+PrintTwoDimArray(twoDimArr);
